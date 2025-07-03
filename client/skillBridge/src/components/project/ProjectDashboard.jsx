e67ca40136d9651d5ade1c04076cce47362461ca
HimanshuMishra-03 <himanshubmishra@gmail.com>
2025-07-03 14:06:51 +0530

    Added token in project

--- client/skillBridge/src/components/project/ProjectDashboard.jsx
+++ client/skillBridge/src/components/project/ProjectDashboard.jsx
@@ -24,7 +24,12 @@ function ProjectDashboard() {
 		const fetchProject = async () => {
 			try {
 				const res = await axios.get(
-					`${API_BASE_URL}/api/projects/project-dashboard/${projectId}`
+					`${API_BASE_URL}/api/projects/project-dashboard/${projectId}`,
+                    {
+						headers: {
+							Authorization: `Bearer ${token}`,
+						},
+					}
 				);
 				setProjectData(res.data.project);
 			} catch (err) {
